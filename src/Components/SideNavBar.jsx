7e310d72bd06bba40c42310a8a35a5ce9a484d32
manpreet711 <manpreetsaini625@gmail.com>
2023-03-21 14:13:32 +0530

    rollback previous change

--- src/Components/SideNavBar.jsx
+++ src/Components/SideNavBar.jsx
@@ -14,30 +14,50 @@ import HelpSection from "./HelpCard";
 
 const SideNavBar = () => {
   return (
-    <>
-      <div className="sidebar">
-        <img
-          style={{ lineHeight: 4 }}
-            src="https://www.atinks.com/wp-content/uploads/2017/10/A.T-inks-logo-600-DPI-1-e1545645137933.png"
-          alt="Profile image"
-          width="44"
-          height="45"
-          className="image--cover"
-        />{" "}
-        AT Links
-        <a href="#home">Dashboard</a>
-        <a href="#news">Orders</a>
-        <a href="#contact">Team Members</a>
-        <a href="#about">Partners</a>
-        <a href="#about">Product Listings</a>
-        <a href="#about">Awards & Honurs</a>
-        <a className="active" href="#about">
-          About Us
-        </a>
-        <a href="#about">Payment info</a>
-        <HelpSection />
-      </div>
-    </>
+    <div className="sidebar">
+      <img
+        style={{ lineHeight: 4 }}
+        src="https://www.atinks.com/wp-content/uploads/2017/10/A.T-inks-logo-600-DPI-1-e1545645137933.png"
+        alt="Profile image"
+        width="44"
+        height="45"
+        className="image--cover"
+      />{" "}
+      AT Links
+      <a href="#home">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faThLarge} />
+        Dashboard
+      </a>
+      <a href="#news">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faArchive} />
+        Orders
+      </a>
+      <a href="#contact">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faUsers} />
+        Team Members
+      </a>
+      <a href="#about">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faHandshake} />
+        Partners
+      </a>
+      <a href="#about">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faList} />
+        Product Listings
+      </a>
+      <a href="#about">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faTrophy} />
+        Awards & Honurs
+      </a>
+      <a className="active" href="#about">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faInfoCircle} />
+        About Us
+      </a>
+      <a href="#about">
+        <FontAwesomeIcon className="pl-5 mx-2" icon={faMoneyBill} />
+        Payment info
+      </a>
+      <HelpSection />
+    </div>
   );
 };
 
