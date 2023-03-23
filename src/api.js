68b2e123da65c6931901876a4b9df671b3e28c8f
manpreet711 <manpreetsaini625@gmail.com>
2023-03-23 11:13:42 +0530

    updated api.js file

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //while running locally
-  //Run npm run vite and change the base url to local url http://127.0.0.1:5173/
+  //Run npm run vite and change the base url to local url http://localhost:3000/
   //to perform update and delete operation on contact as i coudnt find an service to
   //host json file and perform updatee/delete operation
   baseURL: "https://api.npoint.io/470cd19f04f0c20c5d1c/",
